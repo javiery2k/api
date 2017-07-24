4826793fd66947009ccd67c28fbaecb4389799b4
Javier Moran <javiery2k@gmail.com>
2017-07-24 18:43:46 -0500

    no message

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
 var express = require('express');
+var cors = require('cors');
 var app = express();
+const port = 3001;
 
-
-// GET method route
-app.get('/', function (req, res) {
-  res.send('GET request to the homepage');
-});
+app.use(cors());
 
 // POST method route
-app.post('/', function (req, res) {
-  res.send('POST request to the homepage');
+app.get('/login', function(req, res) {
+  const json = {
+    status: 'ok',
+    data: {
+      nombre: 'Javier',
+      apellido: 'Moran',
+      photo: '/img/avatars/6.jpg'
+    }
+  }
+  res.send(json);
 });
 
-app.get('/about', function (req, res) {
-  res.send('about');
+app.get('/session', function(req, res) {
+  const json = {
+    id: 1,
+    nombre: 'Javier',
+    apellido: 'Moran',
+    photo: '/img/avatars/6.jpg'
+  }
+  res.send(json);
+});
+
+app.get('/requisiciones', function(req, res) {
+  res.send('requisiciones');
 });
 
 
-app.listen(3001, function () {
-  console.log('Ready');
+app.listen(3001, function() {
+  console.log('Levantando el servido de nodejs');
+  console.log('API esta listo.');
+  console.log('Ingrese a la siguiente direccion para ver el api http://localhost:' + port);
 });
